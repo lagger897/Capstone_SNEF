a452baede1e0cb1fc73dcd4cdcca8f137f57d4d6
Nhan <Nguyen>
2019-07-01 19:27:37 +0700

    Commit e
    test

--- SNEF_Web/src/main/java/capstone/snef/WebAdmin/controllers/RequestController.java
+++ SNEF_Web/src/main/java/capstone/snef/WebAdmin/controllers/RequestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class RequestController {
-    @RequestMapping("/")
+    @RequestMapping("/web/admin")
     public String home(){
         return "homepage.jsp";
     }
